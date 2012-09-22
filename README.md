a69feb6266ca259a59b3d01de8db07a1280105a3
François Robichet <calvein@gmail.com>
2012-09-22 16:42:25 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,63 @@
+Stickout
+========
+
+Stickout is a [jQuery][] plugin, which works well with the [Twitter bootstrap][], to stick things.
+Stickout is not a polyfill for the new [position: sticky][], it emulates the sticky effect from some mobiles Apps (Google Now, Google+…).
+Just [try it][], scroll down the example and then, scroll up.
+
+Usage
+-----
+
+### Via data attributes
+
+To easily add stickout behavior to an element, just add `data-spy="stickout"` to the element you want to spy on.
+
+```html
+<div data-spy="stickout">...</div>
+```
+
+### Via JavaScript
+
+Call the stickout via JavaScript
+
+```javascript
+$('#el').stickout(options)
+```
+
+### Options
+
+Options can be passed via data attributes or JavaScript. For data attributes, append the option name to `data-`, as in `data-animation=""`.
+
+<table class="table table-bordered table-striped">
+    <thead>
+        <tr>
+            <th style="width: 100px;">Name</th>
+            <th style="width: 50px;">type</th>
+            <th style="width: 200px;">default</th>
+            <th>description</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>boxShadow
+            <td>string
+            <td>'0 5px 10px hsla(0, 0%, 0%, .5)'
+            <td>Apply the css property <code>box-shadow</code> on the element
+        <tr>
+            <td>offset
+            <td>object
+            <td>{ top: 20, hide: 40 }
+            <td>The offset of the element once fixed. <code>top</code> stand for the <code>top</code> position once fixed and <code>hide</code> the position off the
+                element when the user scroll to the top.
+    </tbody>
+</table>
+
+**Heads up!** Options for individual stickout can alternatively be specified through the use of data attributes.
+
+### Methods
+#### .stickout(’destroy’)
+Destroys the stickout behavior on the element
+
+  [jQuery]: http://jquery.com
+  [Twitter bootstrap]: http://getbootstrap.com
+  [position: sticky]: http://updates.html5rocks.com/2012/08/Stick-your-landings-position-sticky-lands-in-WebKit
+  [try it]: http://calvein.github.com/stickout#example
---
+++ scripts/jquery.stickout.js
@@ -0,0 +1,139 @@
+/* ==========================================================
+ * jquery.stickout.js v1.0.0
+ * http://calvein.github.com/stickout
+ * ==========================================================
+ * By François Robichet http://francois.robichet.com or @calvein pretty much everywhere
+ * Released under the WTFPL license
+ * http://sam.zoy.org/wtfpl/
+ *
+ * Date: Sep 14 19:45:00 2012
+ * ========================================================== */
+
+!function($) {
+
+    "use strict"; // Here some spare semi-colon for people who like them ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+   /* STICKOUT CLASS DEFINITION
+    * ========================= */
+
+    var Stickout = function(el, options) {
+        this.options = $.extend({}, $.fn.stickout.defaults, options)
+        this.$el = $(el)
+        this.$fakeEl
+        this.scrollPosition = 0
+        this.$window = $(window).on('scroll.stickout', $.proxy(this.checkPosition, this))
+        this.init()
+        this.checkPosition()
+    }
+
+    Stickout.prototype = {
+
+        constructor: Stickout
+
+      , init: function() {
+            this.$fakeEl = $('<div>').css({
+                width: this.$el.outerWidth(true)
+              , height: this.$el.outerHeight(true)
+            }).insertAfter(this.$el)
+
+            this.$el.css({
+                position: 'absolute'
+              , width: this.$el.width()
+            })
+        }
+
+      , checkPosition: function() {
+            var scrollHeight = $(document).height()
+              , scrollTop = this.$window.scrollTop()
+              , position = this.$el.offset().top + this.$el.outerHeight(true)
+              , fakeTop = this.$fakeEl.offset().top
+
+
+            // Scroll top
+            if (this.scrollPosition > scrollTop) {
+                if (position < scrollTop) {
+                    this.$el
+                    .data('shown', true)
+                    .css({
+                        top: scrollTop - this.options.offset.hide
+                      , boxShadow: this.options.boxShadow
+                    })
+                } else {
+                    // Return at the initial position
+                    if (fakeTop > scrollTop) {
+                        this.$el
+                        .css({
+                            position: 'absolute'
+                          , top: fakeTop
+                          , boxShadow: 'none'
+                        })
+                    // Set the element into fixed position
+                    } else if (scrollTop - this.options.offset.top <= parseInt(this.$el.css('top'), 10)) {
+                        this.$el
+                        .data('fixed', true)
+                        .css({
+                            position: 'fixed'
+                          , top: this.options.offset.top
+                        })
+                    }
+                }
+            // Scroll bottom
+            } else {
+                if (this.$el.data('fixed')) {
+                    this.$el
+                    .data('fixed', false)
+                    .css({
+                        position: 'absolute'
+                      , top: this.$el.offset().top
+                    })
+                }
+            }
+
+            this.scrollPosition = scrollTop
+        }
+
+      , destroy: function() {
+            this.$fakeEl.remove()
+            this.$el.removeAttr('style')
+            this.$window = $(window).off('scroll.stickout')
+        }
+    }
+
+   /* STICKOUT PLUGIN DEFINITION
+    * ========================== */
+
+    $.fn.stickout = function(option) {
+        return this.each(function() {
+          var $this = $(this)
+            , data = $this.data('stickout')
+            , options = typeof option === 'object' && option
+          if (!data) $this.data('stickout', (data = new Stickout(this, options)))
+          if (typeof option === 'string') data[option]()
+        })
+    }
+
+    $.fn.stickout.defaults = {
+        boxShadow: '0 5px 10px hsla(0, 0%, 0%, .5)'
+      , offset: {
+            top: 20
+          , hide: 40
+      }
+    }
+
+   /* STICKOUT PLUGIN DEFINITION
+    * ========================== */
+
+    $(window).on('load', function() {
+        $('[data-spy="stickout"]').each(function() {
+            var $spy = $(this)
+              , data = $spy.data()
+
+            data.offsetBottom && (data.offset.bottom = data.offsetBottom)
+            data.offsetHide && (data.offset.hide = data.offsetHide)
+
+            $spy.stickout(data)
+        })
+    })
+
+
+}(window.jQuery)
